a507c090f3f59e1857d0f3c8fb9f35f5f037949d
雨猫纸 <165914844+xllit@users.noreply.github.com>
2025-12-28 01:31:58 +0800

    index

---
+++ mc/js/main.js
@@ -0,0 +1,32 @@
+const bgLayer = document.getElementById("bg-layer");
+const bgPath = "mc/pic/";
+const bgList = [
+  "bg1.png",
+  "bg2.png",
+  "bg3.png"
+  // 你可以继续加
+];
+
+function randomBg() {
+  const img = bgList[Math.floor(Math.random() * bgList.length)];
+  bgLayer.style.opacity = 0;
+
+  setTimeout(() => {
+    bgLayer.style.backgroundImage = `url(${bgPath + img})`;
+    bgLayer.style.opacity = 1;
+  }, 800);
+}
+
+// 初始随机
+randomBg();
+
+// 每 60 秒轮换
+setInterval(randomBg, 60000);
+
+// 复制 IP
+function copyIP() {
+  const ip = document.getElementById("server-ip").innerText;
+  navigator.clipboard.writeText(ip);
+  alert("IP 已复制！");
+}
+
